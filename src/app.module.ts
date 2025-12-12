b9317f52bd0f8937505744b7c8981c4ed6db8728
aviral-max <aviral.kaushal@maxhealthcare.com>
2025-12-12 11:16:50 +0530

    added auth module and ran the nest app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 
 // A module is a class annotated with a @Module() decorator.
 // Here, we define the root module of the application.
 // AppModule is the main module that NestJS uses to bootstrap the application whose configuration is defined in the @Module() decorator.
 @Module({
-  imports: [],
+  imports: [AuthModule],
   controllers: [],
   providers: [],
 })
